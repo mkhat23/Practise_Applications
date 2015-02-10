d00580898b7eb5d04470272fc2c92481b588fb2f
Khataria, Mandeep <mfiddy23@gmail.com>
2015-02-10 16:53:06 +0000

    Changes for layout and main activity.

--- VineApplication/VineSoundboardApplication/app/src/main/java/com/mandeepk/mkh10/vinesoundboardapplication/MyActivity.java
+++ VineApplication/VineSoundboardApplication/app/src/main/java/com/mandeepk/mkh10/vinesoundboardapplication/MyActivity.java
@@ -1,19 +1,46 @@
 package com.mandeepk.mkh10.vinesoundboardapplication;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import junit.framework.Test;
 
 
 public class MyActivity extends Activity {
+    private Button mNinePlusTenButton;
+    private Button mTwentyOneButton;
+    private Button mDoItForTheVineButton;
+    private MediaPlayer mp;
 
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        mNinePlusTenButton = (Button) findViewById(R.id.nine_plus_ten);
+        mp = MediaPlayer.create(this, R.raw.you_stupid);
+        mNinePlusTenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            mp.start();
+            }
+        });
+
+        mTwentyOneButton = (Button) findViewById(R.id.twenty_one);
+        mp = MediaPlayer.create(this, R.raw.twenty_one);
+        mTwentyOneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mp.start();
+            }
+        });
     }
 
 
