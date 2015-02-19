971389609e3fca187a53eac0dac949153fb2ad30
Khataria, Mandeep <mfiddy23@gmail.com>
2015-02-19 13:11:49 +0000

    changes to get rid of overflow menu

--- VineApplication/VineSoundboardApplication/app/src/main/java/com/mandeepk/mkh10/vinesoundboardapplication/MyActivity.java
+++ VineApplication/VineSoundboardApplication/app/src/main/java/com/mandeepk/mkh10/vinesoundboardapplication/MyActivity.java
@@ -94,11 +94,11 @@ public class MyActivity extends ActionBarActivity {
     };
 
 
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
-    }
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.my, menu);
+//        return true;
+//    }
 
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
